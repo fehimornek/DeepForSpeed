58dec078c61e574d0e219e6d4946ec04348b3f94
fehim <fehimornek@gmail.com>
2022-02-16 21:30:22 +0300

    now seperates data to X, y values while creating the data

--- createData.py
+++ createData.py
@@ -9,7 +9,7 @@ import os
 """
 this script records road, minimap and speed-o-meter with the correct key presses associated with them. 
 I can get around 20fps with this script with i5 9th generation processor. If your fps is less than this
-it may not create enough data or play the game right. To check your fps you can uncomment the lines that show fps
+it may not create enough data or play the game right. To check your fps you can uncomment the lines that show fps.
 """
 
 
@@ -23,7 +23,7 @@ class CreateData:
         self.last_time = time.time()
 
     def key_press(self):
-        onehotencoding = [0,0,0,0,0,0]            # elements of the list on the left corresponds to [w, a, d, wa, wd, nothing]
+        onehotencoding = np.array([0,0,0,0,0,0])  # elements of the list on the left corresponds to [w, a, d, wa, wd, nothing]
         if keyboard.is_pressed("w"):
             if keyboard.is_pressed("a"):
                 onehotencoding[3] = 1
@@ -60,21 +60,25 @@ class CreateData:
                 #   cv2.destroyAllWindows()
 
 
-        # return list of images as an array
-        return [road_arr, minimap_arr, speed_arr]
+        # return list of images as an numpy array
+        return np.array([road_arr, minimap_arr, speed_arr], dtype=object)
 
     def main(self):
         name = input("enter the name for the training file: ")
 
         # get current directory and add the data to the training_data folder
-        file = os.getcwd() + "\\DeepForSpeed\\training_data\\{}.npy".format(name)
+        file = os.getcwd() + "\\DeepForSpeed\\training_data\\{}".format(name)
 
-        if os.path.isfile(file):
-            print("data already exists! appending to the data.")
-            training_data = list(np.load(file, allow_pickle=True))
-        else:
+        if not os.path.exists(file):
             print("new data created!")
-            training_data = []
+            os.mkdir(file)
+            training_data_X = np.array([])
+            training_data_Y = np.array([])
+
+        else:
+            print("data already exists! appending to the data.")
+            training_data_X = np.load(file + f"\\{name}X.npy", allow_pickle=True)
+            training_data_Y = np.load(file + f"\\{name}y.npy", allow_pickle=True)
 
         for i in range(3, 0, -1):
             print("data collection starts in {}".format(i))
@@ -84,12 +88,14 @@ class CreateData:
         while True:
             key = self.key_press()          # key is an one hot encoded array
             image = self.get_screen()       # image is an array of three elements
-            training_data.append([key, image])
+            training_data_X = np.append(training_data_X, image)
+            training_data_Y = np.append(training_data_Y, key)
 
             # code snippet by sentdex <3
-            if len(training_data) % 500 == 0:
-                print("training files saved! frame count: {}".format(len(training_data)))
-                np.save(file, training_data)
+            if len(training_data_X) % 500 == 0:
+                print("training files saved! frame count: {}".format(len(training_data_X)))
+                np.save(file + f"\\{name}X.npy", training_data_X)
+                np.save(file + f"\\{name}Y.npy", training_data_Y)
 
             if keyboard.is_pressed("q"):            # if q is pressed data collection ends
                 break
