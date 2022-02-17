9af3e9ee8e03d94b999c0bf63244b370f5e596c0
fehim <fehimornek@gmail.com>
2022-02-17 13:56:25 +0300

    pytorch model now implemented partially as proof of concept also training is working too

--- model_architectures.py
+++ model_architectures.py
@@ -1,53 +1,92 @@
+import numpy as np
+import torch
 from torch import flatten
 import torch.nn as nn
 import torch.optim as optim
 
 
+# modified nvidia architecture
 class nvidia_arch(nn.Module):
     """
     1- nvidia doesn't mention any activation function in their paper so i took the liberty to use ReLU.
     2- nvidia uses 200x66 resolution image but to make things just a little faster and more memory
     efficient i will be using images of size 185x60
     3- hyperparameters are also chosen by me because they are not mentioned
+    4- network will also do additional convolutions to understand information from minimap and speedometer
     """
+    def __str__(self):
+        return "this is nvidia architecture"
+
     def __init__(self):
         super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=24, kernel_size=(5,5), stride=(2,2)),
+        # conv1 will look at the road, conv2 will look at the minimap and conv3 will look at the speedometer
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=4, out_channels=24, kernel_size=(5,5), stride=(2,2)),
             nn.ReLU(),
             nn.Conv2d(in_channels=24, out_channels=36, kernel_size=(5,5), stride=(2,2)),
             nn.ReLU(),
             nn.Conv2d(in_channels=36, out_channels=48, kernel_size=(5,5), stride=(2,2)),
             nn.ReLU(),
-            nn.Conv2d(in_channels=48, out_channels=64, kernel_size=(3,3)),
+            nn.Conv2d(in_channels=48, out_channels=64, kernel_size=(3,3), stride=(1,1), padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3))
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=(1,1)),
+            nn.Flatten()
         )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=(5,5), stride=(2,2)),
+            nn.Conv2d(in_channels=6, out_channels=12, kernel_size=(5, 5), stride=(2, 2)),
+            nn.Conv2d(in_channels=12, out_channels=18, kernel_size=(5, 5), stride=(2, 2))
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=(5, 5), stride=(2, 2)),
+            nn.Conv2d(in_channels=6, out_channels=12, kernel_size=(5, 5), stride=(2, 2)),
+        )
+
 
         # fully connected dense layers
         self.linear = nn.Sequential(
             # input to the fully connected layer will be a (17x1) image with 64 channels
-            nn.Linear(in_features=17 * 1 * 64, out_features=100),
+            nn.Linear(in_features=1280, out_features=100),
             nn.Linear(in_features=100, out_features=50),
             nn.Linear(in_features=50, out_features=10),
-            nn.Linear(in_features=10, out_features=1)
+            nn.Linear(in_features=10, out_features=6),
+            nn.LogSoftmax()
         )
 
-    def forward(self, x):
-        x = self.conv(x)
-        x = flatten(x)
+    # x1 is road x2 is minimap and x3 is the speedometer
+    def forward(self, x1):
+        x1 = torch.permute(x1,(0, 3, 2, 1))
+        x = self.conv1(x1)
         x = self.linear(x)
         return x
 
-class nvidiaNeuralNet:
-    def __init__(self):
-        self.architecture = nvidia_arch()
-        self.optimizer = optim.Adam(self.architecture.parameters(), lr=0.001)
-        self.epochs = 10
+
+    #def forward(self, x1, x2, x3):
+    #    x1 = self.conv1(x1)
+    #    x2 = self.conv2(x2)
+    #    x3 = self.conv3(x3)
+    #    x1 = flatten(x1)
+    #    x2 = flatten(x2)
+    #    x3 = flatten(x3)
+    #    x = np.concatenate((x1,x2,x3))
+    #    x = self.linear(x)
+    #    return x
 
 
+# i dont really know how i can implement this part right now so it will be commented out for a while
 
-    def train(self, trainset):
-        for epoch in range(self.epochs):
-            for data in trainset:
-
+#class nvidiaNeuralNet:
+#    def __init__(self):
+#        self.architecture = nvidia_arch()
+#        self.optimizer = optim.Adam(self.architecture.parameters(), lr=0.001)
+#        self.epochs = 10
+#
+#
+#
+#    def train(self, trainset):
+#        for epoch in range(self.epochs):
+#            # training set is a list that contains minibatches something like this: [ [[x1,y1]...[x1000,y1000]], [[x1001,y1001]...[x2000,y2000]]... ]
+#            for data in trainset:
+#                # data is a minibatch like [[x1,y1]...[x1000,y1000]]
+#                X, y =
+#
--- train_model.py
+++ train_model.py
@@ -1,3 +1,73 @@
+import numpy as np
+import model_architectures
+import torch.optim as optim
+import torch
+import torch.nn.functional as F
+import os
+import importlib
+import sys
 
+
+def check_file(file):
+    if os.path.exists(file):
+        print("read successful! starting training")
+        return True
+    else:
+        print("cannot read! \n make sure you dont write .npy at the end")
+        return False
+
+def training(neural_net_name, data_folder_name, epochs, batches, optimizer, loss_function, learning_rate = 0.001):
+    # get the specified training data
+    file = os.getcwd() + "\\training_data\\{}".format(data_folder_name)
+    # check if the file exists,
+    # ----->if it does load it
+    # ----->else end the function
+    if check_file(file):
+        training_data_X = np.load(file + f"\\{data_folder_name}X.npy", allow_pickle=True)
+        training_data_Y = list(np.load(file + f"\\{data_folder_name}Y.npy", allow_pickle=True))
+        print("loaded features and labels")
+    else:
+        return
+    # get the specified model from model_architectures
+    class_neuralnet = getattr(model_architectures, neural_net_name)
+    neural_net = class_neuralnet()
+    print("loaded neural network: ", neural_net)
+    # get the specified optimizer from torch.optim class
+    optimizer = getattr(optim, optimizer)(neural_net.parameters(), lr = learning_rate)
+
+    # get the specified loss function from torch.nn.functional class
+    loss_func = getattr(F, loss_function)
+    print("loaded necessary attributes for neural network")
+    road = []
+    minimap = []
+    speed = []
+
+    # seperation of images in training_data_X
+    print("separating data...")
+    for data in training_data_X:
+        road.append(data[0])
+        minimap.append(data[1])
+        speed.append(data[2])
+    print("separation of data successful!")
+    road, minimap, speed, training_data_Y = np.array(road), np.array(minimap), np.array(speed), np.array(training_data_Y)
+    print("transformed list to arrays")
+
+    # training starts here
+    for epoch in range(epochs):
+        print("epoch: ", epoch)
+        permutation = torch.randperm(len(road))
+        for i in range(0, len(road), batches):
+            neural_net.zero_grad()
+            indices = permutation[i:i+batches]
+            road_batch = torch.tensor(road[indices])
+            y_batch = torch.tensor(training_data_Y[indices])
+            y_batch = torch.argmax(y_batch, dim=1)
+            output = neural_net.forward(road_batch/255)  # (, minimap_batch, speed_batch)
+            loss = loss_func(output, y_batch.view(1000,))
+            loss.backward()
+            optimizer.step()
+
+
+training("nvidia_arch", "keke", 10, 1000, "Adam", "nll_loss")
 
 
