0c19530c3100dea209a6df869f439071ec9eedbb
fehim <fehimornek@gmail.com>
2022-02-23 13:11:45 +0300

    deleted an useless comment

--- model_architectures.py
+++ model_architectures.py
@@ -1,9 +1,5 @@
-import numpy as np
 import torch
-from torch import flatten
 import torch.nn as nn
-import torch.optim as optim
-
 
 # modified nvidia architecture
 class nvidia_arch(nn.Module):
@@ -21,7 +17,7 @@ class nvidia_arch(nn.Module):
         super().__init__()
         # conv1 will look at the road, conv2 will look at the minimap and conv3 will look at the speedometer
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=4, out_channels=24, kernel_size=(5,5), stride=(2,2)),
+            nn.Conv2d(in_channels=1, out_channels=24, kernel_size=(5,5), stride=(2,2)),
             nn.ReLU(),
             nn.Conv2d(in_channels=24, out_channels=36, kernel_size=(5,5), stride=(2,2)),
             nn.ReLU(),
@@ -33,7 +29,7 @@ class nvidia_arch(nn.Module):
             nn.Flatten()
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=4, out_channels=6, kernel_size=(5,5), stride=(2,2)),
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=(5,5), stride=(2,2)),
             nn.ReLU(),
             nn.Conv2d(in_channels=6, out_channels=12, kernel_size=(5, 5), stride=(2, 2)),
             nn.ReLU(),
@@ -42,7 +38,7 @@ class nvidia_arch(nn.Module):
 
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=4, out_channels=6, kernel_size=(5, 5), stride=(2, 2)),
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=(5, 5), stride=(2, 2)),
             nn.ReLU(),
             nn.Conv2d(in_channels=6, out_channels=12, kernel_size=(5, 5), stride=(2, 2)),
             nn.Flatten()
@@ -61,30 +57,9 @@ class nvidia_arch(nn.Module):
 
     # x1 is road x2 is minimap and x3 is the speedometer
     def forward(self, x1, x2, x3):
-        x1 = torch.permute(x1,(0, 3, 2, 1))
-        x2 = torch.permute(x2,(0, 3, 2, 1))
-        x3 = torch.permute(x3,(0, 3, 2, 1))
         x1 = self.conv1(x1)
         x2 = self.conv2(x2)
         x3 = self.conv3(x3)
         x = torch.concat((x1,x2,x3), dim=1)
         x = self.linear(x)
         return x
-
-# i dont really know how i can implement this part right now so it will be commented out for a while
-
-#class nvidiaNeuralNet:
-#    def __init__(self):
-#        self.architecture = nvidia_arch()
-#        self.optimizer = optim.Adam(self.architecture.parameters(), lr=0.001)
-#        self.epochs = 10
-#
-#
-#
-#    def train(self, trainset):
-#        for epoch in range(self.epochs):
-#            # training set is a list that contains minibatches something like this: [ [[x1,y1]...[x1000,y1000]], [[x1001,y1001]...[x2000,y2000]]... ]
-#            for data in trainset:
-#                # data is a minibatch like [[x1,y1]...[x1000,y1000]]
-#                X, y =
-#
