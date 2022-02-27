4a0b68a013e25b25e6c5e87c3a5da3821695e207
fehim <fehimornek@gmail.com>
2022-02-27 19:40:36 +0300

    now everything is inside a function and organized well

--- processData.py
+++ processData.py
@@ -1,37 +1,115 @@
 import os
 import numpy as np
-import cv2
 import random
 
-training_dataX = list(np.load("training_data/real/realX.npy", allow_pickle=True))
-training_dataY = list(np.load("training_data/real/realY.npy", allow_pickle=True))
+"""
+The training data is quite imbalanced because most of the time we are just
+pressing the forward key and maybe only half of the time we are steering. If our convnet
+sees this data it will be super biased to just going forward all the time so we need to change that.
+Amount of do nothings is also quite big too this also needs to be taken care of.
+"""
 
-idx =0
-forward, left, right, forright, forleft, nthg = [], [], [], [], [], []
-for data in training_dataX:
-    if training_dataY[idx][0] == 1:
-        forward.append(data)
-        training_dataX.pop(idx)
-        training_dataY.pop(idx)
-    elif training_dataY[idx][4] == 1:
-        forright.append(data)
-    elif training_dataY[idx][5] == 1:
-        nthg.append(data)
-        training_dataX.pop(idx)
-        training_dataY.pop(idx)
-    idx += 1
+def preprocess():
+    data_name = input("which data do you want to preprocess: ")
+    file = os.getcwd() + f"\\training_data\\{data_name}"
 
-random.shuffle(forward)
-random.shuffle(nthg)
-forward = [forward[i] for i in range(len(forright))]
-nthg = [nthg[i] for i in range(len(forright))]
+    if os.path.exists(file):
+        print("loading data!")
+        training_dataX = list(np.load(file + f"\\{data_name}X.npy", allow_pickle=True))
+        training_dataY = list(np.load(file + f"\\{data_name}Y.npy", allow_pickle=True))
+    else:
+        print("data doesnt exist!")
+        return
 
-training_dataX = training_dataX + forward + nthg
+    os.mkdir(os.getcwd() + f"\\training_data\\processed\\{data_name}")
 
-for i in range(len(forward)):
-    training_dataY.append([1,0,0,0,0,0])
-for i in range(len(nthg)):
-    training_dataY.append([0,0,0,0,0,1])
+    forward, right, left, forward_right, forward_left, do_nothing = [], [], [], [], [], []
 
-np.save(os.getcwd() + "\\training_data\\processed\\tX.npy", training_dataX)
-np.save(os.getcwd() + "\\training_data\\processed\\tY.npy", training_dataY)
+    idx = 0
+
+    print("separating data!")
+    # get how many lefts there are because generally left (or right) is pressed the least amount of time. So we
+    # will normalize others based on this
+    countOfLefts = 0
+    for data in training_dataX:
+        # if data is a forward
+        if training_dataY[idx][0] == 1:
+            """
+            append the data and then remove it from training_dataX this is done because
+            we will remove most of the forwards and then add them back to training_dataX 
+            if we dont remove them we will just have even more forwards which would be super bad.
+            """
+            forward.append(data)
+            training_dataX.pop(idx)
+            training_dataY.pop(idx)
+
+        elif training_dataY[idx][1] == 1:
+            left.append(data)
+            training_dataX.pop(idx)
+            training_dataY.pop(idx)
+
+        elif training_dataY[idx][2] == 1:
+            right.append(data)
+            training_dataX.pop(idx)
+            training_dataY.pop(idx)
+
+        elif training_dataY[idx][3] == 1:       # forward lefts
+            forward_left.append(data)
+            training_dataX.pop(idx)
+            training_dataY.pop(idx)
+
+        elif training_dataY[idx][4] == 1:       # forward rights
+            forward_right.append(data)
+            training_dataX.pop(idx)
+            training_dataY.pop(idx)
+
+        elif training_dataY[idx][5] == 1:       # do nothing
+            do_nothing.append(data)
+            training_dataX.pop(idx)
+            training_dataY.pop(idx)
+
+        # used to move on to the next data
+        idx += 1
+
+    random.shuffle(forward),random.shuffle(forward_left),random.shuffle(forward_right)
+    random.shuffle(do_nothing), random.shuffle(left),random.shuffle(right)
+
+    lengths = [len(forward),len(forward_left),len(forward_right),len(do_nothing), len(left), len(right)]
+    minimum_length = min(lengths)
+
+    balanced_forward = [forward[i] for i in range(minimum_length)]
+    balanced_forward_left = [forward_left[i] for i in range(minimum_length)]
+    balanced_forward_right = [forward_right[i] for i in range(minimum_length)]
+    balanced_do_nothing = [do_nothing[i] for i in range(minimum_length)]
+    balanced_left = [left[i] for i in range(minimum_length)]
+    balanced_right = [right[i] for i in range(minimum_length)]
+
+    training_dataX = balanced_forward + balanced_left + balanced_right + \
+                     balanced_forward_left + balanced_forward_right + balanced_do_nothing
+
+    for i in range(countOfLefts):
+        training_dataY.append([1,0,0,0,0,0])
+    for i in range(countOfLefts):
+        training_dataY.append([0,1,0,0,0,0])
+    for i in range(countOfLefts):
+        training_dataY.append([0,0,1,0,0,0])
+    for i in range(countOfLefts):
+        training_dataY.append([0,0,0,1,0,0])
+    for i in range(countOfLefts):
+        training_dataY.append([0,0,0,0,1,0])
+    for i in range(countOfLefts):
+        training_dataY.append([0,0,0,0,0,1])
+
+    permutation = np.arange(len(training_dataX))
+    np.random.shuffle(permutation)
+    training_dataY = np.array(training_dataY)
+    training_dataX = np.array(training_dataX)
+    dataX_shuffled = training_dataX[permutation]
+    dataY_shuffled = training_dataY[permutation]
+
+    np.save(os.getcwd() + f"\\training_data\\processed\\{data_name}\\{data_name}X.npy", dataX_shuffled)
+    np.save(os.getcwd() + f"\\training_data\\processed\\{data_name}\\{data_name}Y.npy", dataY_shuffled)
+    print("saved the data!")
+
+if __name__ == "__main__":
+    preprocess()
