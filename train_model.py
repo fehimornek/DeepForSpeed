43d4159f363419a47082dff2431d35b7a014dcd2
fehim <fehimornek@gmail.com>
2022-03-03 22:01:54 +0300

    now after training loss is visible

--- train_model.py
+++ train_model.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import torch
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 # training the neural network
 def training(neural_net_name,  epochs, batches, learning_rate,neural_net_folder_name = "default"):
@@ -33,6 +34,7 @@ def training(neural_net_name,  epochs, batches, learning_rate,neural_net_folder_
     # get the model from model_architectures
     class_neuralnet = getattr(model_architectures, neural_net_name)
     neural_net = class_neuralnet()
+    neural_net.train()
     device = "cuda" if torch.cuda.is_available() else "cpu"
     neural_net = neural_net.to(device)
 
@@ -48,7 +50,7 @@ def training(neural_net_name,  epochs, batches, learning_rate,neural_net_folder_
         print("model weights initialized!")
 
     optimizer = optim.Adam(neural_net.parameters(), lr=learning_rate)
-    loss_func = torch.nn.NLLLoss()
+    loss_func = torch.nn.CrossEntropyLoss()
 
     # separation of images in training_data_X
     road = []
@@ -63,24 +65,26 @@ def training(neural_net_name,  epochs, batches, learning_rate,neural_net_folder_
 
     # turn the training data to tensor and load them to gpu with .cuda()
     road, minimap, speed, training_data_Y = torch.tensor(road).cuda(), torch.tensor(minimap).cuda(), torch.tensor(speed).cuda(), torch.tensor(training_data_Y).cuda()
-
+    loss_arr = []
     start_time = time.time()
     print("starting training!")
     # training starts here
     for epoch in range(epochs):
         # take random permutation of the data this will be used for mini batch training
         permutation = torch.randperm(len(road))
-        print("epoch: ", epoch+1)
+        print("epoch: ", epoch + 1)
         for i in range(0, len(road), batches):
             # take indices using the random permutation
-            indices = permutation[i:i+batches]
+            indices = permutation[i:i + batches]
             # take batches of data with the indices and then turn them into tensors because numpy arrays can't be fed to the neural net
-            road_batch, minimap_batch, speed_batch, y_batch = road[indices], minimap[indices], speed[indices], training_data_Y[indices].float()
+            road_batch, minimap_batch, speed_batch, y_batch = road[indices], minimap[indices], speed[indices], \
+                                                              training_data_Y[indices].float()
 
             # add a dummy dimension because we dont have a channel value
             road_batch = road_batch[None, :]
             minimap_batch = minimap_batch[None, :]
             speed_batch = speed_batch[None, :]
+
             # turn the data from (channel, batches, width, height ) to (batches, channel, width, height )
             road_batch = torch.permute(road_batch, (1, 0, 2, 3))
             minimap_batch = torch.permute(minimap_batch, (1, 0, 2, 3))
@@ -96,9 +100,10 @@ def training(neural_net_name,  epochs, batches, learning_rate,neural_net_folder_
             we have to turn data into float because pytorch expects data to be float type. This can be achieved by dividing by 255 which also
             regularizes the data.
             """
-            output = neural_net(road_batch/255, minimap_batch/255, speed_batch/255)
+            output = neural_net(road_batch / 255, minimap_batch / 255, speed_batch / 255)
             # calculate the error
             loss = loss_func(output, y_batch)
+            loss_arr.append(loss.detach().cpu().numpy())
             # backprop, thank god pytorch for that am i right? lol
             neural_net.zero_grad()
             loss.backward()
@@ -106,7 +111,11 @@ def training(neural_net_name,  epochs, batches, learning_rate,neural_net_folder_
 
     torch.save(neural_net.state_dict(), os.getcwd() + f"\\trained_models\\{neural_net_folder_name}.pth")
     print("saved neural network weights!")
-    print("training took: ", (time.time() - start_time)/60, "minutes")
+    print("training took: ", (time.time() - start_time) / 60, "minutes")
+    # show how loss changed
+    plt.plot(loss_arr)
+    plt.show()
 
-if __name__== "__main__":
-    training(learning_rate=0.01, epochs=50, neural_net_name="nvidia_arch", batches=32, neural_net_folder_name="default")
+
+if __name__ == "__main__":
+    training(learning_rate=0.01, epochs=500, neural_net_name="nvidia_arch", batches=32, neural_net_folder_name="default")
